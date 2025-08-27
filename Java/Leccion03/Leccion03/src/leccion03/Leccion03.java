efbb21a3ae9196af86ed512dc5eb6c834899a5fc
marcos <homolamarcos@gmail.com>
2025-08-27 20:15:26 -0300

    Terminamos la Clase 3 de Java

---
+++ Java/Leccion03/Leccion03/src/leccion03/Leccion03.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package leccion03;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Leccion03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Atributos de la clase
+        String nombre;
+        String apellido;
+    }
+    
+}
