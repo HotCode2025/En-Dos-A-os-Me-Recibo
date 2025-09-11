af1b19864558a26a25aab8a4c6011959a914ee31
Miguel Perez <miguellrvc@gmail.com>
2025-09-10 22:28:36 -0300

    se realizaron los ejercicios 4.1, 4.2 y 4.3; clase 04 de java

---
+++ Java/Leccion04/src/Clase04/Ejercicio4_1Persona.java
@@ -0,0 +1,13 @@
+package Clase04;
+
+public class Ejercicio4_1Persona {
+  //atributos de la clase (Características)
+  String nombre; 
+  String apellido; 
+  
+  //Métodos de la clase (Acciones)
+  public void obtenerInformacion() {
+      System.out.println("Nombre: "+nombre);
+      System.err.println("ApellidO: "+apellido);
+  }
+}
---
+++ Java/Leccion04/src/Clase04/Ejercicio4_2PruebaPersona.java
@@ -0,0 +1,20 @@
+
+package Clase04;
+
+public class Ejercicio4_2PruebaPersona {
+   public static void main(String[] args) {
+       //creamos un objeto de la clase Ejercicio4_1Persona
+       Ejercicio4_1Persona persona1; 
+       persona1 = new Ejercicio4_1Persona(); //llamamos al constructor
+       persona1.nombre = "Grupo";
+       persona1.apellido = "En dos años me recibo";
+       //ejecutamos el método de persona
+       persona1.obtenerInformacion();
+       
+       //Creamos un nuevo objeto. 
+       Ejercicio4_1Persona persona2 = new Ejercicio4_1Persona(); 
+       //Imprimimos los objetos, lo que obtenemos es su dirección en memoria
+       System.err.println("Persona1 = " + persona1 );
+       System.out.println("Persona2 = " + persona2);
+   } 
+}
