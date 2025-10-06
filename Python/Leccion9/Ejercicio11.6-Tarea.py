be3bd021d5bd4ca66ed34a2332d4d73ff42680a6
marcos <homolamarcos@gmail.com>
2025-10-06 20:45:15 -0300

    Realizo ejercicio 11.6 de PYTHON

--- Python/Leccion9/Ejercicio11.6-Tarea.py
+++ Python/Leccion9/Ejercicio11.6-Tarea.py
@@ -1,16 +1,43 @@
+#Definir una clase padre llamada vehiculo y dos clases hijas llamadas
+#auto y bicicleta, las cuales heredan de la clase padre vehiculo. 
+#la clase padre debe tener los siguientes atributos y metodos:
+
+#vehiculo (clase padre)
+#atributos: color, ruedas
+# metodos: (__init__(color, ruedas), y __str__( ))
+
+#auto (clase hija de vehiculo)
+#atributos:(velocidad(km/h))
+#metodos: (__init__(color, ruedas, velocidad), y __str__( ))
+
+#bicicleta (clase hija de vehiculo)
+#atributos:(tipo(urbana/montaña/etc.))
+#metodos: (__init__(color, ruedas, tipo), y __str__( ))
+
+#crear un objeto de cada clase
+
 class Vehiculo:
-    '''
-    Definir una clase padre llamada Vehiculo y dos clases hijas llamadas
-    Auto y Bicicleta, las cuales heredan de la clase padre Vehiculo. La clase
-    padre debe tener los siguientes atributos y métodos:
-    Vehiculo (clase padre)
-    -Atributos(color, ruedas)
-    -Métodos (____init__(color, ruedas) y ___str__())
-    Auto(clase hija de Vehiculo)
-    -Atributos (velocidad (km/hr))
-    -Métodos(____init__(color, ruedas, velocidad) y ___str___())
-    Bicicleta (clase hija de Vehiculo)
-    -Atributos (tipo(urbana/montaña/etc.)
-    -Métodos(____init__(color, ruedas, tipo) y ___str__()
-    Crear un objeto de cada clase
-    '''
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+    def __str__(self):
+        return f"Color: {self.color}, Ruedas: {self.ruedas}"
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+    def __str__(self):
+        return f"Color: {self.color}, Ruedas: {self.ruedas}, Velocidad: {self.velocidad}"
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+    def __str__(self):
+        return f"Color: {self.color}, Ruedas: {self.ruedas}, Tipo: {self.tipo}"
+
+auto = Auto("azul", 4, 120)
+print(auto)
+bici = Bicicleta("rojo", 2, "montaña")
+print(bici)
